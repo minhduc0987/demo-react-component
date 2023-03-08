911765f7cfe7df3fbb13882c5a1bb29ef7b06a7b
Duc Nguyen2 <duc.nguyen2@niteco.se>
2023-03-08 15:10:55 +0700

    Updates

---
+++ precache-manifest.30b5c91141dd265211c57e397be523ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe043cf983fd062f467da2d7fc13b0b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "53f02bf7f8b92f16dbf3",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "c41489559c5632b66d56",
+    "url": "./static/js/2.38b3799d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.38b3799d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53f02bf7f8b92f16dbf3",
+    "url": "./static/js/main.1922a7b6.chunk.js"
+  },
+  {
+    "revision": "895a00ea2e581af5fd32",
+    "url": "./static/js/runtime-main.06e47784.js"
+  }
+]);
